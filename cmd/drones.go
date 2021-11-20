69f9bee7cae87b9b9d67525d2a853dd4d6f8ac14
sheilaqf <sheilaqf>
2021-11-20 08:11:16 -0500

    elmininating the use of the "internal" directory

--- cmd/drones.go
+++ cmd/drones.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	. "github.com/sheilaqf/drones/internal/pkg/drone"
+)
+
+var (
+	registeredDrones map[string]Drone // use SerialNumber as key
+)
+
 func main() {
 
 }
---
+++ pkg/drone/drone.go
@@ -0,0 +1,17 @@
+package drone
+
+import (
+	"drones/pkg/medication"
+)
+
+type (
+	//define what is a drone
+	Drone struct {
+		SerialNumber     string                  `json:"serial_number"`    // (100 characters max);
+		Model            string                  `json:"model"`            // (Lightweight, Middleweight, Cruiserweight, Heavyweight);
+		WeightLimit      uint16                  `json:"weight_limit"`     // (500gr max);
+		BatteryCapacity  uint8                   `json:"battery_capacity"` // (percentage);
+		State            string                  `json:"state"`            // (IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING).
+		LoadedMedication []medication.Medication `json:"loaded_medication"`
+	}
+)
